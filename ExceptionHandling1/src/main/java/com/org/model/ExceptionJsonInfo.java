31c0ab3fe084f0bfc25cdacce19da1a00a7f11ee
akash <akashchaudhari935@gmail.com>
2019-01-21 12:27:36 +0530

    Exception Handling

---
+++ ExceptionHandling1/src/main/java/com/org/model/ExceptionJsonInfo.java
@@ -0,0 +1,24 @@
+package com.org.model;
+
+public class ExceptionJsonInfo {
+
+	private String url;
+	private String message;
+
+	public String getUrl() {
+		return url;
+	}
+
+	public void setUrl(String url) {
+		this.url = url;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+}
---
+++ ExceptionHandling1/src/main/webapp/index.jsp
@@ -0,0 +1,10 @@
+<html>
+<body>
+<h2>Hello World!</h2>
+
+
+<a href="home.jsp">Click here</a>
+
+
+</body>
+</html>
